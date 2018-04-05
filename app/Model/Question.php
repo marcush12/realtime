963573a2d29252c390d4cf7012d03d291ea47feb
vagrant <vagrant@homestead>
2018-04-05 22:39:58 +0000

    CRUD for Question

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -9,6 +9,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    //protected $fillable = ['title', 'slug', 'body', 'category_id', 'user_id'];
+    protected $guarded = [];
+
     public function user()//question belongs to a user
     {
         return $this->belongsTo(User::class);
@@ -22,5 +30,10 @@ class Question extends Model
     public function category()//question belongs to a user
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
